da1a71238a27c12eb30d2ba31baec65b16ef4778
Mrzhangy <item_group@163.com>
2017-11-21 14:57:03 +0800

    添加日志中间件

--- middleware/index.js
+++ middleware/index.js
@@ -5,7 +5,13 @@ const staticFiles = require('koa-static')
 
 const miSend = require('./mi-send')
 
+// 引入日志中间件
+const miLog = require('./mi-log')
+
 module.exports = (app) => {
+    // 注册日志中间件
+    app.use(miLog())
+    
     // 指定public目录为静态资源目录，用来存放js css images等
     app.use(staticFiles(path.resolve(__dirname, "../public")))
 
---
+++ middleware/mi-log/logger.js
@@ -0,0 +1,40 @@
+const log4js = require('log4js')
+const methods = ['track', 'debug', 'info', 'warn', 'error', 'fatal', 'mark']
+
+module.exports = (options) => {
+    const contextLogger = {}
+    const config = {
+        cheese: {
+            type: 'dateFile', // 日志类型 
+            filename: `logs/task`,  // 输出的文件名
+            pattern: '-yyyy-MM-dd.log',  // 文件名增加后缀
+            alwaysIncludePattern: true   // 是否总是有后缀名
+        },
+        categories: {
+            default: {
+                appenders: ['cheese'],
+                level: 'info'
+            }
+        }
+    }
+    
+    const logger = log4js.getLogger('cheese')
+
+    return async (ctx, next) => {
+        const start = Date.now()
+        
+        log4js.configure(config)
+        // 循环methods将所有方法挂载到ctx上
+        methods.forEach((method, i) => {
+            contextLogger[method] = (message) => {
+                logger[method](message)
+            }
+        })
+        ctx.log = contextLogger
+
+        await next()
+        const end = Date.now()
+        const responseTime = end - start
+        logger.info(`响应时间${responseTime/1000}s`)
+    }
+}
